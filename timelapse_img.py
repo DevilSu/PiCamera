369c50a03e0926d576e8f2af7fa5672f80a803f8
DevilSu <swimm1rsu@gmail.com>
2017-11-02 16:55:45 +0800

    shorten delay

--- timelapse_img.py
+++ timelapse_img.py
@@ -6,4 +6,4 @@ camera.start_preview()
 sleep(2)
 for filename in camera.capture_continuous('img{counter:03d}.jpg'):
     print('Captured %s' % filename)
-    sleep(300) # wait 5 minutes
+    sleep(2) # wait 2 seconds
